aa81cef7da86147e0d1f042ebf4cb27bdbf83b75
Hostlife21 <sima.serafim2@mail.ru>
2022-01-09 15:43:14 +0300

    init lesson7

--- lesson7/task1/webpack.config.js
+++ lesson7/task1/webpack.config.js
@@ -1,34 +1,25 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /.js$/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /.s?css$/,
+        test: /\.s?css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /.(jpg|png$)/,
+        test: /\.(png|jpg|gif)$/i,
         use: [
           {
             loader: 'url-loader',
             options: {
-              limit: 8192,
-              name: '[name].[ext]',
               outputPath: 'images',
+              limit: 8192,
             },
           },
         ],
@@ -36,8 +27,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
